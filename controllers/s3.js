8db6305c7518f0e8a643900cdf5d3593496ca6f9
Gregory Williams <gregoryew@gmail.com>
2020-10-17 21:42:02 -0700

    test

--- controllers/s3.js
+++ controllers/s3.js
@@ -1,10 +1,8 @@
 const AWS = require('aws-sdk');
 
 const s3 = new AWS.S3({
-  apiVersion: "2010-12-01",
   accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: "us-east-2"
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
 const uploadFile = (bucketName, fileName, query) => {
