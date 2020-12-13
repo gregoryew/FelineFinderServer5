7bb60c3aebafb0bf1892e6f3cc6f7014ffddd4e0
Gregory Williams <gregoryew@gmail.com>
2020-12-13 02:16:52 -0800

    downloads cert from spaces

--- controllers/sendPush.js
+++ controllers/sendPush.js
@@ -5,10 +5,10 @@ const fs = require('fs');
 
 sendPushTest = function(deviceToken) {
 
-  if (!fs.exists(appRoot() + 'controllers/AuthKey_6P7YN9TBQF.p8')) {
+  if (!fs.exists(appRoot + 'controllers/AuthKey_6P7YN9TBQF.p8')) {
     s3.downloadFile('ff-saved-queries', 'AuthKey_6P7YN9TBQF.p8')
     .then(function(data) {
-      console.log('Writting cert to ' + appRoot() + 'controllers/AuthKey_6P7YN9TBQF.p8');
+      console.log('Writting cert to ' + appRoot + 'controllers/AuthKey_6P7YN9TBQF.p8');
       fs.writeFileSync(appRoot() + 'controllers/AuthKey_6P7YN9TBQF.p8')
     })
     .catch (function (error) {
@@ -18,7 +18,7 @@ sendPushTest = function(deviceToken) {
 
 var options = {
     token: {
-    key: appRoot() + 'controllers/' + process.env.apnKey,
+    key: appRoot + 'controllers/' + process.env.apnKey,
     keyId: process.env.apnKeyId,
     teamId: process.env.apnTeamID
     },
