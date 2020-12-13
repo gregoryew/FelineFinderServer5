bccdd584f4d39e6d174f740f5ffce35a1ac2ff8b
Gregory Williams <gregoryew@gmail.com>
2020-12-13 04:46:15 -0800

    downloading certificate from s3 into workspace

--- controllers/sendPush.js
+++ controllers/sendPush.js
@@ -5,16 +5,18 @@ const s3 = require('./s3');
 const fs = require('fs');
 
 sendPushTest = function(deviceToken) {
-  s3.downloadFile('ff-saved-queries', process.env.apnKey, function(data, error) {
-    fs.writeFileSync("/root/controllers/" + process.env.apnKey, data)
-    sendPush(deviceToken)
-  })
+  if (!fs.exists(appRoot + '/controllers' + process.env.apnKey)) {
+    s3.downloadFile('ff-saved-queries', process.env.apnKey, function(data, error) {
+      fs.writeFileSync(appRoot + '/controllers' + process.env.apnKey, data)
+      sendPush(deviceToken)
+    })
+  }
 }
 
 sendPush = function(deviceToken) {
 var options = {
   token: {
-  key: '/root/controllers/' + process.env.apnKey,
+  key: appRoot + '/controllers' + process.env.apnKey,
   keyId: process.env.apnKeyId,
   teamId: process.env.apnTeamID
   },
