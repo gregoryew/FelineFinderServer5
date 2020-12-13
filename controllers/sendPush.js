a18036e1901608cc80e5a200e88991b9863cdf11
Gregory Williams <gregoryew@gmail.com>
2020-12-13 05:13:17 -0800

    corrected path for apn

--- controllers/sendPush.js
+++ controllers/sendPush.js
@@ -5,18 +5,18 @@ const s3 = require('./s3');
 const fs = require('fs');
 
 sendPushTest = function(deviceToken) {
-  fs.exists(appRoot + '/controllers' + process.env.apnKey, (exists) => {
+  fs.exists(appRoot + process.env.apnKey, (exists) => {
     if (!exists) {s3.downloadFile('ff-saved-queries', process.env.apnKey, function(error, data) {
-      fs.writeFileSync(appRoot + '/controllers' + process.env.apnKey, data)
+      fs.writeFileSync(appRoot + process.env.apnKey, data)
       sendPush(deviceToken)
     })}
-  }) 
+  })
 }
 
 sendPush = function(deviceToken) {
 var options = {
   token: {
-  key: appRoot + '/controllers' + process.env.apnKey,
+  key: appRoot + process.env.apnKey,
   keyId: process.env.apnKeyId,
   teamId: process.env.apnTeamID
   },
