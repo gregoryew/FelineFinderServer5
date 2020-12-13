21335e72bfbe7f87f9d5ed666e17a2ace1bed9a4
Gregory Williams <gregoryew@gmail.com>
2020-12-13 04:41:55 -0800

    made download a promise

--- controllers/sendPush.js
+++ controllers/sendPush.js
@@ -1,16 +1,24 @@
 const AWS = require('aws-sdk');
 let apn = require('@parse/node-apn');
 let appRoot = require('app-root-path');
+const s3 = require('./s3');
+const fs = require('fs');
 
 sendPushTest = function(deviceToken) {
-  
+  s3.downloadFile('ff-saved-queries', process.env.apnKey, function(data, error) {
+    fs.writeFileSync("/root/controllers/" + process.env.apnKey, data)
+    sendPush(deviceToken)
+  })
+}
+
+sendPush = function(deviceToken) {
 var options = {
-    token: {
-    key: '/root/controllers/' + process.env.apnKey,
-    keyId: process.env.apnKeyId,
-    teamId: process.env.apnTeamID
-    },
-    production: false
+  token: {
+  key: '/root/controllers/' + process.env.apnKey,
+  keyId: process.env.apnKeyId,
+  teamId: process.env.apnTeamID
+  },
+  production: false
 };
 
 var apnProvider = new apn.Provider(options);    
@@ -25,9 +33,9 @@ note.payload = {'messageFrom': 'John Appleseed'};
 note.topic = "com.gregsiosapps.TestAPN"
 
 apnProvider.send(note, deviceToken).then( (result) => {
-    console.log("RESULT = " + JSON.stringify(result));
-    // see documentation for an explanation of result
-  });
+  console.log("RESULT = " + JSON.stringify(result));
+  // see documentation for an explanation of result
+});
 }
 
 module.exports.sendPushTest = sendPushTest
