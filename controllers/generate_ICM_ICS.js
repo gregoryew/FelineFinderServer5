4a964682375147a80b5a75826f2e59f9ba070c01
Gregory Williams <gregoryew@gmail.com>
2021-02-16 23:36:35 -0800

    Corrected an error

--- controllers/generate_ICM_ICS.js
+++ controllers/generate_ICM_ICS.js
@@ -3,5 +3,5 @@ module.exports = function(app) {
     app.get('/api/schedule', function(req, res) {
         console.writeln(JSON.stringify(schedule));
         json.send(schedule);
-    }
+    })
 }
