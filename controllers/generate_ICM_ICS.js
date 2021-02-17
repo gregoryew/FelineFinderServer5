16b339b52a2c4ab279b18d02cf267dbec4710235
Gregory Williams <gregoryew@gmail.com>
2021-02-16 23:44:33 -0800

    Corrected an error

--- controllers/generate_ICM_ICS.js
+++ controllers/generate_ICM_ICS.js
@@ -1,7 +1,10 @@
-const schedule = require('schedule');
+const bodyParser = require('body-parser');
+const schedule = require('./schedule.js');
 module.exports = function(app) {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
     app.get('/api/schedule', function(req, res) {
         console.writeln(JSON.stringify(schedule));
-        json.send(schedule);
+        res.send(schedule);
     })
 }
